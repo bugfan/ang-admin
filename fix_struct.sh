9d786d49411b59a5f7fc5b0eb2515fcff0ccb7ba
bugfan <908958194@qq.com>
2026-08-11 16:11:51 +0800

    ready ang admin

--- fix_struct.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i '' 's/type EmbedClient struct {/type EmbedClient struct {\n\tclient *openai.Client\n\tmodel  string\n\tisGemini bool\n\tgeminiKey string\n}\n\ntype EmbedClientOld struct {/g' internal/rag/rag.go
